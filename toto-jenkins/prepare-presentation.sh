4714a4d312fcdda294e184553f3d1a24fde40375
Frank MARSHALL <frank@readresolve.io>
2025-11-05 11:55:03 +0100

    Added rm / cp dist

--- toto-jenkins/prepare-presentation.sh
+++ toto-jenkins/prepare-presentation.sh
@@ -2,6 +2,7 @@
 
 readonly SCRIPT=$0
 readonly ENV_FILE_PATH=$1
+readonly TOTO_PRESENTATION_DEPLOY_DIR=$2
 
 echo " > Executing: ${SCRIPT}"
 
@@ -17,6 +18,11 @@ echo " > Generate artifacts with npm:"
 npm --prefix ./toto-presentation/ install
 
 # .env.production
+echo " > Copy tmp env file to ./toto-presentation/.env.production"
 cp $ENV_FILE_PATH ./toto-presentation/.env.production
 
 npm --prefix ./toto-presentation/ run build
+
+echo " > Deploy dist/* to ${TOTO_PRESENTATION_DEPLOY_DIR}"
+rm -r $TOTO_PRESENTATION_DEPLOY_DIR/*
+cp -r ./toto-presentation/dist/* $TOTO_PRESENTATION_DEPLOY_DIR
