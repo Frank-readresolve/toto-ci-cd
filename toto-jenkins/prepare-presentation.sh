85fbb38d7e0d68ed082a773cbbe87878b4846453
Frank MARSHALL <frank@readresolve.io>
2025-11-05 10:27:02 +0100

    Added generate artifacts

--- toto-jenkins/prepare-presentation.sh
+++ toto-jenkins/prepare-presentation.sh
@@ -4,3 +4,10 @@ echo " > Preparing presentation artifacts with node and npm:"
 
 node -v
 npm -v
+
+echo " > Generate artifacts with npm:"
+
+# => toto-presentation/
+
+npm --prefix ./toto-presentation/ install
+npm --prefix ./toto-presentation/ run build
