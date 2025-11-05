17dc8ab563c5254f7afbcc27d023ca03d0db2869
Frank MARSHALL <frank@readresolve.io>
2025-11-05 11:37:42 +0100

    Copy env file

--- toto-jenkins/prepare-presentation.sh
+++ toto-jenkins/prepare-presentation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+readonly SCRIPT=$0
+readonly ENV_FILE_PATH=$1
+
+echo " > Executing: ${SCRIPT}"
+
 echo " > Preparing presentation artifacts with node and npm:"
 
 node -v
@@ -10,4 +15,8 @@ echo " > Generate artifacts with npm:"
 # => toto-presentation/
 
 npm --prefix ./toto-presentation/ install
+
+# .env.production
+cp $ENV_FILE_PATH ./toto-presentation/.env.production
+
 npm --prefix ./toto-presentation/ run build
