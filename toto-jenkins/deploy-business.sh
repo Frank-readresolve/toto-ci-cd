df6d423e92ff54d21099ce91bcd473d9c02a7baa
Frank MARSHALL <frank@readresolve.io>
2025-11-06 16:05:18 +0100

    Added business deploy

---
+++ toto-jenkins/deploy-business.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+readonly SCRIPT=$0
+readonly TOTO_BUSINESS_DEPLOY_DIR=$1
+readonly TOTO_BUSINESS_URL=$2
+
+echo " > Executing: ${SCRIPT}"
+
+echo " > Deploy target/*.jar to ${TOTO_BUSINESS_DEPLOY_DIR}"
+rm -r $TOTO_BUSINESS_DEPLOY_DIR/*
+cp ./toto-business/target/*.jar $TOTO_BUSINESS_DEPLOY_DIR/toto-business.jar
+
+echo " > Deployed! Have a look, enjoy your job:"
+echo $TOTO_BUSINESS_URL
