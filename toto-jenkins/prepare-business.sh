2d6df2edbb0e95a88552cdcafafc6feca2c90e29
Frank MARSHALL <frank@readresolve.io>
2025-11-06 15:22:15 +0100

    Added properties

--- toto-jenkins/prepare-business.sh
+++ toto-jenkins/prepare-business.sh
@@ -9,6 +9,10 @@ echo " > Executing: ${SCRIPT}"
 
 echo " > Preparing business artifacts with maven and java:"
 
+# application-prod.properties
+echo " > Copy tmp env file to ./toto-business/src/main/resources/application-prod.properties"
+cp $ENV_FILE_PATH ./toto-business/src/main/resources/application-prod.properties
+
 mvn -v
 
 echo " > Generate artifacts with maven:"
