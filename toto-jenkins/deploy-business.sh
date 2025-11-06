499b1be2f84ae9befa42cb2a2e9814f51f58cdb8
Frank MARSHALL <frank@readresolve.io>
2025-11-06 16:30:13 +0100

    Force rm

--- toto-jenkins/deploy-business.sh
+++ toto-jenkins/deploy-business.sh
@@ -9,7 +9,7 @@ readonly TOTO_BUSINESS_URL=$2
 echo " > Executing: ${SCRIPT}"
 
 echo " > Deploy target/*.jar to ${TOTO_BUSINESS_DEPLOY_DIR}"
-rm -r $TOTO_BUSINESS_DEPLOY_DIR/*
+rm -rf $TOTO_BUSINESS_DEPLOY_DIR/*
 cp ./toto-business/target/*.jar $TOTO_BUSINESS_DEPLOY_DIR/toto-business.jar
 
 echo " > Deployed! Have a look, enjoy your job:"
