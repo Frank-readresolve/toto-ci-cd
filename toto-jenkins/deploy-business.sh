cc80dae28c6148e92b934316de7c4e13c1099693
Frank MARSHALL <frank@readresolve.io>
2025-11-07 11:55:10 +0100

    Added start

--- toto-jenkins/deploy-business.sh
+++ toto-jenkins/deploy-business.sh
@@ -21,14 +21,17 @@ if [ $LSOF_EXIT_CODE -eq 0 ]; then
     readonly PID=$(lsof -ti tcp:$TOTO_BUSINESS_PORT)
     echo "Killing gracefully the process with PID=${PID}..."
     kill -15 $PID
-    echo "Process killed"
+    echo " > Process killed"
 else
-    echo "No process running"
+    echo " > No process running"
 fi
 
 echo " > Deploy target/*.jar to ${TOTO_BUSINESS_DEPLOY_DIR}"
 rm -rf $TOTO_BUSINESS_DEPLOY_DIR/*
 cp ./toto-business/target/*.jar $TOTO_BUSINESS_DEPLOY_DIR/toto-business.jar
+
+# nohup: no hang up
+nohup java -jar -Dspring.profiles.active=prod $TOTO_BUSINESS_DEPLOY_DIR/toto-business.jar &
 
 echo " > Deployed! Have a look, enjoy your job:"
 echo $TOTO_BUSINESS_URL
