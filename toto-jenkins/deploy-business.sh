7214a8fff6add84ecea5dfced4cdb7094c52cb97
Frank MARSHALL <frank@readresolve.io>
2025-11-07 10:37:41 +0100

    Added stop app

--- toto-jenkins/deploy-business.sh
+++ toto-jenkins/deploy-business.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-set -e
-
 readonly SCRIPT=$0
 readonly TOTO_BUSINESS_DEPLOY_DIR=$1
 readonly TOTO_BUSINESS_URL=$2
+readonly TOTO_BUSINESS_PORT=$3
 
 echo " > Executing: ${SCRIPT}"
+
+echo " > Stopping toto-business (optional operation):"
+
+readonly PID=$(lsof -ti tcp:$TOTO_BUSINESS_PORT)
+readonly PID_EXIT_CODE = $?
+# Comment why...
+set -e
+
+if [ $PID_EXIT_CODE -eq 0]; then
+    # Stop the app
+    echo "Killing gracefully the process with PID=${PID}..."
+    kill -15 $PID
+    echo "Process killed"
+elif
+    echo "No process running"
+fi
 
 echo " > Deploy target/*.jar to ${TOTO_BUSINESS_DEPLOY_DIR}"
 rm -rf $TOTO_BUSINESS_DEPLOY_DIR/*
