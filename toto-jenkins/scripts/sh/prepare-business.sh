d94ae2250e3e07843bc93e5b4060acbbcbc6f542
Frank MARSHALL <frank@readresolve.io>
2026-01-02 17:53:30 +0100

    Renamed TEST to INT_TEST (bash issue)

--- toto-jenkins/scripts/sh/prepare-business.sh
+++ toto-jenkins/scripts/sh/prepare-business.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-readonly TEST=$1
+readonly INT_TEST=$1
 readonly ENV_FILE_PATH=$2
 readonly TEST_ENV_FILE_PATH=$3
 
 echo "#############"
-echo "TEST=${TEST}"
+echo "INT_TEST=${INT_TEST}"
 echo "TEST_ENV_FILE_PATH=${TEST_ENV_FILE_PATH}"
 echo "#############"
 
