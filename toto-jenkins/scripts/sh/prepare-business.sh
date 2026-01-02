d75b0540ef20e9c499e33427c25ccd31dc63de4a
Frank MARSHALL <frank@readresolve.io>
2026-01-02 18:44:56 +0100

    Fixing

--- toto-jenkins/scripts/sh/prepare-business.sh
+++ toto-jenkins/scripts/sh/prepare-business.sh
@@ -13,4 +13,4 @@ cp $TEST_ENV_FILE_PATH ./toto-business/src/test/resources/application-test.prope
 
 echo " > Execute goals (skip_test=${SKIP_TEST}) with maven:"
 mvn -v
-mvn -Dmaven.test.skip=$SKIP_TEST -f ./toto-business/pom.xml package
+mvn -Dmaven.test.skip=$SKIP_TEST -Dtest=* -f ./toto-business/pom.xml package
