285d6ebb864109c83c00abd26b8f40240f602617
Frank MARSHALL <frank@readresolve.io>
2026-01-02 18:42:58 +0100

    Fixing

--- toto-jenkins/scripts/sh/prepare-business.sh
+++ toto-jenkins/scripts/sh/prepare-business.sh
@@ -5,14 +5,12 @@ readonly ENV_FILE_PATH=$1
 readonly SKIP_TEST=$2
 readonly TEST_ENV_FILE_PATH=$3
 
-echo "#############"
-echo "SKIP_TEST=${SKIP_TEST}"
-echo "TEST_ENV_FILE_PATH=${TEST_ENV_FILE_PATH}"
-echo "#############"
-
 echo " > Copy tmp env file to ./toto-business/src/main/resources/application-prod.properties"
 cp $ENV_FILE_PATH ./toto-business/src/main/resources/application-prod.properties
 
-echo " > Execute goals with maven:"
+echo " > Copy tmp test env file to ./toto-business/src/test/resources/application-test.properties"
+cp $TEST_ENV_FILE_PATH ./toto-business/src/test/resources/application-test.properties
+
+echo " > Execute goals (skip_test=${SKIP_TEST}) with maven:"
 mvn -v
-mvn -Dmaven.test.skip=true -f ./toto-business/pom.xml package
+mvn -Dmaven.test.skip=$SKIP_TEST -f ./toto-business/pom.xml package
