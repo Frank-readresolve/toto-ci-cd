11484875ace27cad4b0ad4466f6ee61faa992ed6
Frank MARSHALL <frank@readresolve.io>
2026-01-03 17:29:22 +0100

    Adding reports

--- toto-jenkins/scripts/sh/prepare-business.sh
+++ toto-jenkins/scripts/sh/prepare-business.sh
@@ -3,10 +3,11 @@ set -e
 
 readonly ENV_FILE_PATH=$1
 readonly SKIP_TEST=$2
+readonly SONAR_TOKEN=$3
 
 echo " > Copy tmp env file to ./toto-business/src/main/resources/application-prod.properties"
 cp $ENV_FILE_PATH ./toto-business/src/main/resources/application-prod.properties
 
 echo " > Execute goals (skip_test=${SKIP_TEST}) with maven:"
 mvn -v
-mvn -Dmaven.test.skip=$SKIP_TEST -Dtest=* -f ./toto-business/pom.xml package jacoco:report
+mvn -Dmaven.test.skip=$SKIP_TEST -Dtest=* -f ./toto-business/pom.xml package jacoco:report sonar:sonar -Dsonar.login=$SONAR_TOKEN
